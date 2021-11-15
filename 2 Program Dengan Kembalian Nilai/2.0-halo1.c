f62cb84286fde0bce5e5617ac3f4908a5a694f5c
Muhamad Bestagi Romadhon <40150664+bestagi@users.noreply.github.com>
2021-11-16 06:27:06 +0700

    Move File to Dir and update Hallo 2

---
+++ 2 Program Dengan Kembalian Nilai/2.0-halo1.c
@@ -0,0 +1,14 @@
+/* File halo1.c */
+/* Penulis : Bestagi, Email bestagi.egi@live.com */
+/* Program untuk Menuliskan Halo Dunia! ke layar */
+
+#include<stdio.h>
+
+int
+main(){
+    /* KAMUS */
+
+    /* ALGORITMA */
+    printf ("Halo Dunia\n");
+    return 0;
+}
