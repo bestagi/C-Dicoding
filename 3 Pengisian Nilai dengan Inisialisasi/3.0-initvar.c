29361fcb5292334084aa99f767efc6eaa3e3e673
Muhamad Bestagi Romadhon <40150664+bestagi@users.noreply.github.com>
2021-11-17 11:16:09 +0700

    int var

---
+++ 3 Pengisian Nilai dengan Inisialisasi/3.0-initvar.c
@@ -0,0 +1,14 @@
+/* File : 3.0-initvar.c */
+/* Penenulis : Bestagi, Email bestagi.egi@live.com */
+/* Mengisi variavel "i" yang bertipe int dengan nilai "5" dan menuliskannya */
+
+#include<stdio.h>
+int main(){
+/* Kamus */
+    int i = 20 - 1 ; /* Deklarasi dan Inisialisasi nilai variabel "i" dengan "5" */
+    
+    /* Algoritma */
+    printf("Ini nilai i : %d \n", i); /* Untuk mengamati dampak inisialisasi */ 
+    
+    return 0;
+}
