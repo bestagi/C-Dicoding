993db96f8288588769c780c1ead19a508cb81885
Muhamad Bestagi Romadhon <40150664+bestagi@users.noreply.github.com>
2021-11-24 13:16:28 +0700

    Baca Tulis

---
+++ 5 Baca-Tulis/5.0-bacaNum.c
@@ -0,0 +1,6 @@
+/* File : 5.0-bacaNum.c */
+/* Penulis : Bestagi, Email bestagi.egi@live.com */
+/* Deskripsi :
+              Contoh membaca nilai numerik : bilangan bulat, bilangan rill
+              kemudian menuliskan nilai yang dibaca */
+
