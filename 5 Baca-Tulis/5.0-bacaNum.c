6b2b4f44416133a506e8eedc0127f653ab1753d9
Muhamad Bestagi Romadhon <40150664+bestagi@users.noreply.github.com>
2021-11-24 14:10:34 +0700

    baca tulis nomer

--- 5 Baca-Tulis/5.0-bacaNum.c
+++ 5 Baca-Tulis/5.0-bacaNum.c
@@ -4,3 +4,23 @@
               Contoh membaca nilai numerik : bilangan bulat, bilangan rill
               kemudian menuliskan nilai yang dibaca */
 
+#include<stdio.h>
+int main(){
+    /* Kamus */
+    int angka;
+    float x;
+
+    /* Program */
+    printf("Contoh membaca dan menulis, ketik nilai integer : ");
+    scanf("%d", &angka); /* membaca nilai a yang bertype integer perhatikan bahwa nama variabel ditulis dengan "&a" */
+    printf("Nilai yang dibaca : %d \n", angka);
+
+    printf("Ketik nilai sebuah bilangan riil : ");
+    scanf("%f", &x); /* Membaca nilai a yang bertype integer */
+    printf("Nilai yang dibaca : %f \n", x);
+
+    /* coba ketik : scanf ("%d", a);dan tuliskan nilainya. Apa akibatnya ?*/
+    /* coba ketik : scanf ("%f", x);dan tuliskan nilainya. Apa akibatnya ?*/
+
+    return 0;
+}
